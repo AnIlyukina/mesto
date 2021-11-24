f77975cc95719acab53e6f226011ce3ccc0d5aa2
AnnaIluykina <anyailyukina@yandex.ru>
2021-11-24 22:16:22 +0800

    попап с картинкой

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -7,7 +7,7 @@ export class PopupWithImage extends Popup {
     this._popupElementTitle = this._popupElement.querySelector(".popup__image-title")
   }
 
-  open = ({ element }) => {
+  open = (element) => {
     super.open()
     this._popupElementCard.src = element.link
     this._popupElementTitle.textContent = element.name
